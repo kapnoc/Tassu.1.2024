75a2b0654457a6cec6ef485b8617facb97341166
Tanguy Gérôme <tanguy.gerome@gmail.com>
2024-06-11 10:53:30 +0300

    Update build scripts to compress assets that are above 512kb

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-# ./compress_assets.sh
+./compress_assets.sh
 xelatex tassu12024.tex
 ./compress_pdf.sh
--- compress_assets.sh
+++ compress_assets.sh
@@ -1,5 +1,5 @@
 
-for file in ./assets/*.jpg
+for file in `find ./assets/*.jpg -type f -size +512k`
 do
 	convert "$file" -define jpeg:extent=512kb "$file"
 done
